d45d0151ea0dd6f457220ea35ecc9329f0a6e524
TelAranRhiod <aarozywang@163.com>
2022-03-21 19:00:44 -0400

    slight changes

--- Godot_proj/Player/Player.cs
+++ Godot_proj/Player/Player.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using Godot;
 using Array = Godot.Collections.Array;
 using Vector2 = Godot.Vector2;
