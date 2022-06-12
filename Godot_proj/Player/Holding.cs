8f6f042b6347404f0a86126228a82fffec052fcf
TelAranRhiod <aarozywang@163.com>
2022-06-11 22:23:59 -0400

    player auto lockon aim

--- Godot_proj/Player/Holding.cs
+++ Godot_proj/Player/Holding.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
+using System.Collections;
+using Godot.Collections;
+using Array = Godot.Collections.Array;
 
 public class Holding : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    public Mob_type_1 target;
+    private Boolean lockon = false;
     private ItemSlot cur;
     private Item hold;
     // Called when the node enters the scene tree for the first time.
@@ -19,9 +21,43 @@ public class Holding : Node2D
         LookAt(GetGlobalMousePosition());
     }
 
+    public override void _Process(float delta)
+    {
+        Area2D targetAquire = GetNode("aim_region") as Area2D;
+        Array n = targetAquire.GetOverlappingBodies();
+        double dis = Double.PositiveInfinity;
+        foreach (var v in n)
+        {
+            
+            if (v is Mob_type_1 && 
+                (v as Mob_type_1).Position.DistanceTo(GetGlobalMousePosition()) < dis)
+            {
+                dis = (v as Mob_type_1).Position.DistanceTo(GetGlobalMousePosition());
+                target = v as Mob_type_1;
+            }
+        }
+
+        if (dis == Double.PositiveInfinity)
+        {
+            target = null;
+        }
+    }
+
+    private void get_target()
+    {
+        LookAt(target.Position);
+    }
+
     public override void _PhysicsProcess(float delta)
     {
-       get_mouse();
+        if (IsInstanceValid(target))
+        {
+            get_target();
+        }
+        else
+        {
+            get_mouse();
+        }
     }
 
     public void _holding_item(ItemSlot slot)
@@ -56,7 +92,7 @@ public class Holding : Node2D
         }
         bullet.setVelocity(100.0f);
         
-        bullet.Transform = (GetNode("AimSight") as Position2D).GlobalTransform;
+        bullet.Transform = (GetNode("aim_region") as Area2D).GlobalTransform;
         GetTree().CurrentScene.AddChild(bullet);
         bullet.Position = new Vector2(bullet.Position.x - 25, bullet.Position.y - 15);
         GD.Print(bullet.Position.ToString());
